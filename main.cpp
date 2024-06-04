0cd62e697b3d204016e1345f72c6e7a6675aa373
Zach <zachisaunders12@gmail.com>
2024-06-04 18:22:04 -0400

    It compiles now

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <map>
 using namespace std;
 
 class Solution {
-  private:
-    int charHash(char letter) {
-      if (letter > 91) {
-        return (int) (letter - 97);
-      }
-      else {
-        return (int) (letter - 65);
-      }
-    }
   public:
     int longestPalindrome(string s) {
-      int charCounts[26] = { 0 };
+      map<char, int> charMap;
       for (int i = 0; i < size(s); i++) {
-        int index = charHash(s[i]);
-        charCounts[index]++;
+        charMap[s[i]]++; 
       }
+      int palinSize = 0;
+      for (auto const& charItem : charMap) {
+        if (charItem.second < 2) {
+          continue;
+        }
+        else {
+          palinSize += charItem.second;
+        }
+      }
+      delete &charMap;
+      return palinSize;
     }
 };
+
+int main() {
+  Solution sol = Solution();
+  int num = sol.longestPalindrome("abcccccdd");
+  cout << "Num: " << num << '\n';
+  return 0;
+}
