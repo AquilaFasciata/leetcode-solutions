c6c98ff6f1347c76b3575831e48c8a8d54c807d4
Zach <zachisaunders12@gmail.com>
2024-06-05 21:12:02 -0400

    Did some work

--- main.cpp
+++ main.cpp
@@ -6,19 +6,41 @@ using namespace std;
 
 class Solution {
 public:
+    int charHash(char letter) {
+        return ((int) letter - (int) 'A');
+    }
     vector<string> commonChars(vector<string>& words) {
-      vector<map<char, int>> charCounts;
-      charCounts.reserve(words.size());
-      for (auto const& word : words) {
-        map<char, int> letterFreqMap;
-        for (char const& letter : word) {
-          letterFreqMap[letter]++;
+        vector<int> lowestCount('z' - 'A', __INT_MAX__);
+        for (string const& word : words) {
+            // Get the lowest count of each character in each string
+            vector<int> counts(56, 0);
+            for (char const& character : word) {
+                    counts[charHash(character)]++;
+            }
+            for (int i = 0; i < counts.size(); i++) {
+                if (counts[i] < lowestCount[i]) {
+                    lowestCount[i] = counts[i];
+                }
+            }
         }
-        charCounts.push_back(letterFreqMap);
-      }
-    // Right now there's a vector with a map of the frequency of each character.
-    // Next, logic to check what the lowest count of each character in the arrays are;
-    // then add the characters as needed to the map
+
+            // Build the string vector for returning
+            vector<string> returnedVector;
+            returnedVector.reserve(5);
+            for(int i = 0; i < 'z'; i++) {
+                if (lowestCount[i] > 150000) {
+                    continue;
+                }
+
+                for (int j = 0; j < lowestCount[i]; j++) {
+                  if (lowestCount[i] - 1 == 0) {
+                    continue;
+                  }
+                char character = (char) (i + 'A');
+                returnedVector.push_back(string(1, character));
+                }
+            }
+            return returnedVector;
     }
 };
 
@@ -37,5 +59,6 @@ int main() {
   for (auto const& item : num2) {
     cout << "\"" << item << "\", ";
   }
+  cout << "]\n";
   return 0;
 }
