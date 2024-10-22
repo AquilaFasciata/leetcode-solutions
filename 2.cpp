335888646f2b56ce1de99fdeeb2080a355ca4c27
Zach Saunders <zachisaunders12@gmail.com>
2024-10-22 19:22:43 -0400

    Added a lot, can't be bothered to debug it right now

--- 2.cpp
+++ 2.cpp
@@ -1,3 +1,7 @@
+#include <cmath>
+#include <numeric>
+#include <string>
+#include <vector>
 struct ListNode {
   int val;
   ListNode *next;
@@ -8,5 +12,34 @@ struct ListNode {
 
 class Solution {
 public:
-  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {}
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    int num1 = 0;
+    int num2 = 0;
+    int i = 0;
+    while (l1->next != nullptr) {
+      num1 += (l1->val * std::pow(10, i));
+      i++;
+      l1 = l1->next;
+    }
+    i = 0;
+    while (l2->next != nullptr) {
+      num1 += (l2->val * std::pow(10, i));
+      i++;
+      l2 = l2->next;
+    }
+    int result = num1 + num2;
+    std::string result_str = std::to_string(result);
+
+    ListNode *new_list = new ListNode();
+
+    for (char letter : result_str) {
+      char temp_letter[] = {letter, '\0'};
+      int num = std::atoi(temp_letter);
+      new_list = new ListNode(num, new_list);
+    }
+
+    return new_list;
+  }
 };
+
+int main(int argc, char *argv[]) { return 0; }
