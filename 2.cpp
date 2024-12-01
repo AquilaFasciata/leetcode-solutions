6a4f3cd979a33642a43107875c848da37c3225bd
Zach <zachisaunders12@gmail.com>
2024-11-30 21:16:59 -0500

    Number 2 is too big, have to implemnt (probably) long addition or the like

--- 2.cpp
+++ 2.cpp
@@ -28,30 +28,38 @@ std::ostream &operator<<(std::ostream &os, const ListNode *node) {
 class Solution {
 public:
   ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    int num1 = 0;
-    int num2 = 0;
-    int i = 0;
+    long long int num1 = 0;
+    long long int num2 = 0;
+    long long int i = 0;
     while (l1->next != nullptr) {
       num1 += (l1->val * std::pow(10, i));
       i++;
       l1 = l1->next;
     }
+    num1 += (l1->val * std::pow(10, i));
+    std::cout << "num1: " << num1 << '\n';
     i = 0;
     while (l2->next != nullptr) {
-      num1 += (l2->val * std::pow(10, i));
+      num2 += (l2->val * std::pow(10, i));
       i++;
       l2 = l2->next;
     }
-    int result = num1 + num2;
-    std::cout << "Result is " << result << '\n';
+    num2 += (l2->val * std::pow(10, i));
+    std::cout << "num2: " << num2 << '\n';
+    long long int result = num1 + num2;
+    std::cout << "The result is " << result << " \n";
     std::string result_str = std::to_string(result);
 
-    ListNode *new_list = new ListNode();
+    ListNode *new_list = nullptr;
 
     for (char letter : result_str) {
       char temp_letter[] = {letter, '\0'};
       int num = std::atoi(temp_letter);
-      new_list = new ListNode(num, new_list);
+      if (new_list != nullptr) {
+        new_list = new ListNode(num, new_list);
+      } else {
+        new_list = new ListNode(num);
+      }
     }
 
     return new_list;
@@ -68,12 +76,14 @@ ListNode *build_list(std::vector<int> nums) {
 
 int main(int argc, char *argv[]) {
   Solution sol = Solution();
-  std::vector<int> nums = {9, 9, 9, 9, 9, 9, 9, 9, 9, 1};
-  std::vector<int> num2 = {9};
+  std::vector<int> nums = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+  std::vector<int> num2 = {4, 6, 5};
   ListNode *big_num = build_list(nums);
   ListNode *small = build_list(num2);
   std::cout << small << '\n';
-  ListNode *new_nums = sol.addTwoNumbers(big_num, small);
+  ListNode other = ListNode(9);
+  ListNode *new_nums = sol.addTwoNumbers(big_num, &other);
 
   std::cout << new_nums;
 
