d92b65fa32c9a148cf813d1e0a3ca5f0ec3348c4
Zach <zachisaunders12@gmail.com>
2025-05-18 22:49:16 -0400

    Did #2 successfully

--- 2.cpp
+++ 2.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstddef>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -28,62 +29,51 @@ std::ostream &operator<<(std::ostream &os, const ListNode *node) {
 class Solution {
 public:
   ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    long long int num1 = 0;
-    long long int num2 = 0;
-    long long int i = 0;
-    while (l1->next != nullptr) {
-      num1 += (l1->val * std::pow(10, i));
-      i++;
-      l1 = l1->next;
-    }
-    num1 += (l1->val * std::pow(10, i));
-    std::cout << "num1: " << num1 << '\n';
-    i = 0;
-    while (l2->next != nullptr) {
-      num2 += (l2->val * std::pow(10, i));
-      i++;
-      l2 = l2->next;
-    }
-    num2 += (l2->val * std::pow(10, i));
-    std::cout << "num2: " << num2 << '\n';
-    long long int result = num1 + num2;
-    std::cout << "The result is " << result << " \n";
-    std::string result_str = std::to_string(result);
+    static ListNode *dummy_node = new ListNode(0);
+    std::vector<int> sum_vec;
+    sum_vec.reserve(105);
+    int carry = 0;
+    do {
+      int val1 = l1 == nullptr ? 0 : l1->val;
+      int val2 = l2 == nullptr ? 0 : l2->val;
+      int sum = val1 + val2 + carry;
+      carry = sum > 9 ? sum / 10 : 0;
 
-    ListNode *new_list = nullptr;
-
-    for (char letter : result_str) {
-      char temp_letter[] = {letter, '\0'};
-      int num = std::atoi(temp_letter);
-      if (new_list != nullptr) {
-        new_list = new ListNode(num, new_list);
-      } else {
-        new_list = new ListNode(num);
+      sum_vec.push_back(sum % 10);
+      if (l1 != nullptr) {
+        l1 = l1->next;
       }
-    }
+      if (l2 != nullptr) {
+        l2 = l2->next;
+      }
+    } while (l1 != nullptr || l2 != nullptr);
 
-    return new_list;
+    ListNode *list = nullptr;
+    for (int i = sum_vec.size() - 1; i > -1; i--) {
+      list = new ListNode(sum_vec[i], list);
+    }
+    return list;
   }
 };
 
-ListNode *build_list(std::vector<int> nums) {
+ListNode *build_list(std::vector<int> &nums) {
   ListNode *new_list = nullptr;
-  for (const int num : nums) {
-    new_list = new ListNode(num, new_list);
+  for (auto num = nums.end() - 1; num != nums.begin() - 1; --num) {
+    new_list = new ListNode(*num, new_list);
   }
   return new_list;
 }
 
 int main(int argc, char *argv[]) {
   Solution sol = Solution();
-  std::vector<int> nums = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
-  std::vector<int> num2 = {4, 6, 5};
+  std::vector<int> nums = {1, 2, 3};
+  std::vector<int> num2 = {1, 2, 3};
   ListNode *big_num = build_list(nums);
   ListNode *small = build_list(num2);
+  std::cout << big_num << '\n';
   std::cout << small << '\n';
   ListNode other = ListNode(9);
-  ListNode *new_nums = sol.addTwoNumbers(big_num, &other);
+  ListNode *new_nums = sol.addTwoNumbers(big_num, small);
 
   std::cout << new_nums;
 
