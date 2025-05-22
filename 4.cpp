7480e00764326d5fffb730a29763f5cd93392eda
Zach <zachisaunders12@gmail.com>
2025-05-22 19:50:14 -0400

    That did not work as planned

--- 4.cpp
+++ 4.cpp
@@ -1,16 +1,15 @@
 #include <cassert>
-#include <cstddef>
 #include <vector>
 using std::vector;
 
-struct Value {
+struct sValue {
   int val;
   bool exists = true;
-  Value(int num) {
+  sValue(int num) {
     val = num;
     exists = true;
   }
-  Value() {
+  sValue() {
     exists = false;
     val = 0;
   }
@@ -18,12 +17,12 @@ struct Value {
   void clear();
 };
 
-void Value::set(int num) {
+void sValue::set(int num) {
   this->exists = true;
   this->val = num;
 }
 
-void Value::clear() {
+void sValue::clear() {
   this->exists = false;
   this->val = 0;
 }
@@ -33,22 +32,24 @@ public:
   double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
     assert(true == 1);
     int total_len = nums1.size() + nums2.size();
+    cout << total_len / 2 << '\n';
     vector<int> merged;
     merged.reserve(total_len / 2 + 3);
     short odd_num = (total_len % 2) == 1;
     vector<int>::iterator iter1 = nums1.begin(), iter2 = nums2.begin();
-    for (int i = 0; i < total_len + odd_num; i++) {
-      Value num1, num2;
-      try {
-        num1.set(*iter1);
-      } catch (...) {
+    for (int i = 0; i < (total_len / 2) + odd_num; i++) {
+      sValue num1, num2;
+      num1.set(*iter1);
+      num2.set(*iter2);
+
+      if (iter1 + 1 != nums1.end())
+        iter1++;
+      else
         num1.clear();
-      }
-      try {
-        num2.set(*iter2);
-      } catch (...) {
+      if (iter2 + 1 != nums2.end())
+        iter2++;
+      else
         num2.clear();
-      }
 
       assert(num1.exists || num2.exists);
       if (num1.exists && num2.exists) {
@@ -66,5 +67,14 @@ public:
           merged.push_back(num2.val);
       }
     }
+
+    if (!odd_num) {
+      auto sum = *(merged.end() - 1);
+      sum += *(merged.end() - 2);
+      return ((double)sum) / 2.0;
+    } else {
+      return ((double)*(merged.end() - 1));
+    }
   }
 };
+;
